bfb331d64732a563693fba05bd9d594b58eb1ecf
Gabrielle <gabrielle.mi@hotmail.com>
2024-01-08 21:35:59 -0500

    fix: fix links and visual

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,6 +1,6 @@
 export const sharedBtnStyles = [
-    "py-3 z-10 text-base leading-none font-bold transition-color rounded-full uppercase bg-yellow relative overflow-hidden",
-    "after:h-full after:delay-75 after:-z-10 after:transition-[width] after:absolute after:left-0 after:w-0 after:top-0 after:rounded-full",
-    "before:h-full before:-z-10 before:transition-[width] before:absolute before:left-0 before:w-0 before:top-0 before:rounded-full",
+    "py-3 z-20 leading-none font-bold transition-color rounded-full uppercase bg-yellow relative overflow-hidden",
+    "after:h-full after:delay-75 after:-z-20 after:transition-[width] after:absolute after:left-0 after:w-0 after:top-0 after:rounded-full",
+    "before:h-full before:-z-20 before:transition-[width] before:absolute before:left-0 before:w-0 before:top-0 before:rounded-full",
     "hover:after:w-full hover:before:w-full hover:text-yellow"
 ].join(' ');
